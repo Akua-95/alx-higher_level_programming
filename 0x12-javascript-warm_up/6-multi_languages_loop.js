bce569d4f3e4642748edef285c04521006ca22cb
Akua-95 <serwaa.precious@gmail.com>
2024-01-09 12:51:03 -0800

    Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const sentence = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const len = sentence.length;
+let i = 0;
+for (; i < len; i++) {
+  console.log(sentence[i]);
+}
