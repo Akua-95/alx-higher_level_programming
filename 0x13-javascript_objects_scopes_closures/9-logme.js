28f52ebc82bdc44c260ef7a0d8c02af58bf2569e
Akua-95 <serwaa.precious@gmail.com>
2024-01-11 14:06:32 -0800

    Log me

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let count = 0;
+let narg = 0;
 
-exports.logMe = function(item) {
-  console.log(count + ": " + item);
-  count++;
+exports.logMe = function (item) {
+  console.log(narg + ': ' + item);
+  narg++;
 };
