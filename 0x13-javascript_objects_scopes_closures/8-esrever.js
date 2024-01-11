726b70d69f9193e7cfbb03a68062f2ee2b817f80
Akua-95 <serwaa.precious@gmail.com>
2024-01-11 14:03:52 -0800

    Esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,13 @@
-Write a function that returns the reversed version of a list:
-
-Prototype: exports.esrever = function (list)
-You are not allow to use the built-in method reverse
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let len = list.length - 1;
+  let i = 0;
+  while ((len - i) > 0) {
+    const aux = list[len];
+    list[len] = list[i];
+    list[i] = aux;
+    i++;
+    len--;
+  }
+  return list;
+};
