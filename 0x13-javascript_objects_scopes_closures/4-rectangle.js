421f1f4c6a403080de16b0d12b047c9788fd38fa
Akua-95 <serwaa.precious@gmail.com>
2024-01-11 13:58:54 -0800

    Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,37 +1,29 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return {};
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
-
-  print() {
-    if (!this.width || !this.height) {
-      console.log("Empty Rectangle");
-    } else {
-      const row = "X".repeat(this.width);
-      for (let i = 0; i < this.height; i++) {
-        console.log(row);
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += 'X';
       }
+      console.log(s);
     }
   }
-
-  rotate() {
-    [this.width, this.height] = [this.height, this.width];
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
   }
-
-  double() {
+  double () {
     this.width *= 2;
     this.height *= 2;
   }
 }
 
-const rect1 = new Rectangle(5, 3);
-rect1.print(); // Prints the rectangle
-rect1.rotate(); // Rotates the rectangle
-rect1.print(); // Prints the rotated rectangle
-rect1.double(); // Doubles the dimensions of the rectangle
-rect1.print(); // Prints the doubled rectangle
+module.exports = Rectangle;
